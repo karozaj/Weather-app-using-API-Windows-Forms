26013479539518c708b6693ab5d59e5bfd01192e
tomek <tomek@DESKTOP-NNS06K6>
2024-06-04 23:23:47 +0200

    Dodaj pliki projektów.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> _cities;
+        private WeatherInfo _weatherInfo;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _cities = new List<string> { "Warsaw", "Berlin", "Copenhagen","Athens","Madrid","Helsinki","Budapest","Brussels","Amsterdam","Dublin","Paris","Lisbon",
+            "Rome","Vienna","Bern","London","Nicosia","Sofia","Stockholm","Tirana","Luxembourg"};
+            comboBoxCity.ItemsSource = _cities;
+            comboBoxCity.SelectedIndex = 0;
+            _weatherInfo = new WeatherInfo();
+        }
+
+        private async void buttonGetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            WeatherData? weatherData = await _weatherInfo.GetWeather(comboBoxCity.Text);
+            if (weatherData != null)
+            {
+                textBlockInfo.Text =($"Opis: {weatherData?.Weather[0].Description}\n");
+                textBlockInfo.Text += ($"Temperatura: {weatherData.Main.Temp}\n");
+                textBlockInfo.Text += ($"Ciśnienie: {weatherData.Main.Pressure}\n");
+                textBlockInfo.Text += ($"Wilgotność: {weatherData.Main.Humidity}");
+                imageWeather.Source = new BitmapImage(new Uri(_weatherInfo.ImageLink));
+            }
+            else
+            {
+                textBlockInfo.Text = "Błąd pobierania danych";
+            }
+            //imageWeather.Source = new BitmapImage(new Uri("http://openweathermap.org/img/w/10d.png"));
+        }
+    }
+}
---
+++ WpfApp2/WeatherInfo.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+public class WeatherInfo
+{
+    private string _apiKey= "";
+    private string _url="https://api.openweathermap.org/data/2.5/weather";
+    private string _imageUrl="http://openweathermap.org/img/w/";
+    public string ImageLink { get; set; }
+    private readonly HttpClient _httpClient;
+
+    public WeatherInfo()
+    {
+        
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<WeatherData?> GetWeather(string city)
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"{this._url}?q={city}&appid={this._apiKey}&units=metric&lang=pl");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+            //WeatherData weatherData = new WeatherData();
+            //weatherData.response = responseBody;
+            WeatherData? weatherData = JsonConvert.DeserializeObject<WeatherData>(responseBody);
+            ImageLink = _imageUrl + weatherData.Weather[0].Icon + ".png";
+            Console.WriteLine(ImageLink);
+            return weatherData;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return null;
+        }
+    }
+
+
+}
+
+public class WeatherData
+{
+    public WeatherMain? Main { get; set; }
+    public WeatherDesc[]? Weather { get; set; }
+    //public string response { get; set; }
+}
+
+public class WeatherDesc
+{
+    public string? Description { get; set; }
+    public string? Icon { get; set; }
+}
+
+public class WeatherMain
+{
+    public float Temp { get; set; }
+    public float Pressure { get; set; }
+    public float Humidity { get; set; }
+}
